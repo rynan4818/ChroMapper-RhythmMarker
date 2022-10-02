2d2d272856c6a05c2fa832ed093404d5f8a4952d
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2022-10-02 11:57:25 +0900

    ChroMapperプラグインの基本処理追加

---
+++ ChroMapper-RhythmMarker/Component/RhythmMarkerController.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace ChroMapper_RhythmMarker.Component
+{
+    public class RhythmMarkerController : MonoBehaviour
+    {
+    }
+}
---
+++ ChroMapper-RhythmMarker/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using ChroMapper_RhythmMarker.Component;
+
+namespace ChroMapper_RhythmMarker
+{
+    [Plugin("Rhythm Marker")]
+    public class Plugin
+    {
+        public static RhythmMarkerController rhythmMarkerController;
+        [Init]
+        private void Init()
+        {
+            SceneManager.sceneLoaded += SceneLoaded;
+            Debug.Log("Rhythm Marker Plugin has loaded!");
+        }
+        [Exit]
+        private void Exit()
+        {
+            Debug.Log("Rhythm Marker:Application has closed!");
+        }
+        private void SceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            if (arg0.buildIndex != 3) // Mapper scene
+                return;
+            if (rhythmMarkerController != null && rhythmMarkerController.isActiveAndEnabled)
+                return;
+            rhythmMarkerController = new GameObject("RhythmMarker").AddComponent<RhythmMarkerController>();
+        }
+    }
+}
